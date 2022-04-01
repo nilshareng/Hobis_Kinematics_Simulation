7e51c03bf42434d9dc8e2ffbbb01e2eb8b8279fe
nilshareng <75070956+nilshareng@users.noreply.github.com>
2022-04-01 10:58:11 +0200

    Update AdaptMarkers.m

--- Functions/AdaptMarkers.m
+++ Functions/AdaptMarkers.m
@@ -1,4 +1,8 @@
 function NewMarkers = AdaptMarkers(tmpMarkers,TargetMarkers)
+% Entries : Two Markers struct. tmpMarkers contains more fields than the TargetMarkers. 
+% TargetMarkers MUST at least contain 
+
+% Outs : A new Marker struct with the fields of TargetMarkers
 % - Cuts down the number of fields of 'tmpMarkers' to the same as 'TargetMarkers'
 % - Renames the fields of 'tmpMarkers' to the same as 'TargetMarkers'
 % 
